342212907be6829d18c3ac44a1b971305c48a932
simba0808 <simba960808@gmail.com>
2024-03-04 07:35:50 +1000

    fix: layout(navbar and footer) update

--- frontend/src/components/user/Sidebar.jsx
+++ frontend/src/components/user/Sidebar.jsx
@@ -1,6 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { LogoImage } from "../../assets";
 
 const Sidebar = ({ handleSlide }) => {
   const [slideShow, setSlideShow] = useState(0);
@@ -19,16 +18,10 @@ const Sidebar = ({ handleSlide }) => {
     <>
       <aside 
         id="sidebar"
-        className={`z-20 w-[230px] shadow-md xs:w-[250px] fixed h-[100%] top-0 overflow-y-auto bg-white -translate-x-full lg:block lg:translate-x-0 transition duration-200 ease-in-out flex-shrink-0 `}
+        className={`z-10 w-[230px] shadow-md xs:w-[250px] fixed h-[100%] top-0 overflow-y-auto bg-white -translate-x-full lg:block lg:translate-x-0 transition duration-200 ease-in-out flex-shrink-0 `}
       >
         <div className="py-4 text-gray-500">
-          <Link
-            to={"/dashboard"}
-            className="ml-2 mt-4 text-lg font-bold text-gray-800 flex items-center justify-center"
-          >
-            <img className="w-[80%]" src={LogoImage} alt="" />
-          </Link>
-          <ul className="mt-14">
+          <ul className="mt-20">
             <li className={`relative px-6 py-3  ${slideShow === 0 ? "bg-purple-100 rounded-lg" : ""}`} onClick={handleSlide}>
               {slideShow === 0 ? (
                 <span
@@ -42,7 +35,7 @@ const Sidebar = ({ handleSlide }) => {
                 to={"/guide"}
                 className="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-400"
               >
-                <svg className="w-[22px] w-[22px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <svg className="w-[22px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 9H5a1 1 0 0 0-1 1v4c0 .6.4 1 1 1h6m0-6v6m0-6 5.4-3.9A1 1 0 0 1 18 6v12.2a1 1 0 0 1-1.6.8L11 15m7 0a3 3 0 0 0 0-6M6 15h3v5H6v-5Z" />
                 </svg>
                 <span className="ml-4 text-md">Orientações</span>
